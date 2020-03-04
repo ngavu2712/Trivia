36b8d789519ae219379ee52fc981e5cc82ce6437
Anna Vu <ngavu2712@gmail.com>
2020-03-04 14:30:53 -0700

    Restructure html page

---
+++ assets/javascript/app.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+
+var questions = [
+
+        { //Question, Options and Answer Object
+    
+            question: "A foreign exchange student lived with Michael when he was young. What did the foreign exchange student take from Michael back to what was formerly Yugoslavia?" , 
+            answers: {
+                a: "All of his blue jeans" , 
+                b: "All of his short" , 
+                c: "His favorite shoes"
+            } ,
+            correctAnswer: "All of his blue jeans"
+        } ,
+        
+        {
+            question: "What does Kevin suggest Dwight put in his gun holster?" , 
+            answers: {
+                a: "A cell phone" ,
+                b: "A banana" , 
+                c: "A toy gun"
+            } , 
+            correctAnswer: "A banana"
+        } , 
+    
+        {
+            question: "At Phyllis and Bob Vance's wedding, what did Michael say everyone called Phyllis back in high school?" ,
+            answers: {
+                a: "Less-urban Aunt Jemima" ,
+                b: "Phyll" , 
+                c: "Easy rider" 
+            } ,
+            correctAnswer: "Easy rider"
+        } , 
+    
+        {
+            question: "What kind of sandwich does Michael have a dream about when he's the head of Michael Scott Paper Company?" ,
+            answers: {
+                a: "Peanut butter and tunafish" ,
+                b: "Tunafish and skittles" , 
+                c: "Mayo and black olives" 
+            } ,
+            correctAnswer: "Peanut butter and tunafish"
+        } ,
+    
+        {
+            question: "What did Kevin buy for himself when he got himself for Secret Santa?" ,
+            answers: {
+                a: "A foot massage" ,
+                b: "A foot bath" , 
+                c: " M&M’s"
+            } ,
+            correctAnswer: "A foot bath"
+        } ,
+    
+        {
+            quesiton: "What's Stanley's morning 3 by 5?" ,
+            answers: {
+                a: "Iced tea 3 sugars 5 creams" ,
+                b: "Coffee 3 sugars 5 creams" , 
+                c: "Tea 3 sugar 5 creams"
+            } ,
+            correctAnswer: "Iced tea 3 sugars 5 creams"
+        } ,
+    
+        {
+            question: "What's in the thermos that Michael offers Pam during morning deliveries for the Michael Scott Paper Company" ,
+            answers: {
+                a: "Milk and sugar" , 
+                b: "Cream and sugar" , 
+                c: "Coffee and sugar" 
+            } ,
+            correctAnswer: "Milk and sugar"
+        } , 
+    
+        {
+            question: "What brand is Michael wearing on the day he accidentally wearing a woman's suit?" ,
+            answers: {
+                a: "MISSsterious" , 
+                b: "MISStery" , 
+                c: "Sassypant" 
+            } ,
+            correctAnswer: "MISSsterious"
+        } ,
+    
+        {
+            question: "What does Kevin say after Kelly yells that her middle name is Rajnigandha and that she hates it?" ,
+            answers: {
+                a: "What is Rajnigandha?" ,
+                b: "What an awful name" , 
+                c: "I thought Rajnigandha was a boy’s name" 
+            } ,
+            correctAnswer: "I thought Rajnigandha was a boy’s name"
+        } ,
+    
+        {
+            question: "Who does Michael wear on his head during the activity on diversity day?" , 
+            answers: {
+                a: "Colin Powell" ,
+                b: "Ghandi" , 
+                c: "Martin Luther King Jr"
+            } ,
+            correctAnswer: "Martin Luther King Jr" 
+        } ];
+
+
+
+
+})
