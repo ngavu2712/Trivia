d22e055e0848a0c5d8896037cb8bb29dd14f0219
Anna Vu <ngavu2712@gmail.com>
2020-03-04 18:14:04 -0700

    changed the design of the game and roughly complete javascript, have to debug cuz app.js did not run correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,47 @@
+/* Player click the Start button, question box display with a list of 10 questions and Start button hide 
+Users choose their answer then hit Submit button, question box hide and result box display
+ */
+
+ /* VARIABLE:
+    Create question variable with question list
+    Create timeCount variable with timer set to 15second
+    Create Result variable with display of correct/incorrect/unanswered 
+
+    FUNCTION:
+    Create questionDisplay function, this function loop through the question variable and append those question list to #game div. 
+    Create countTime function, this function setInterval -1 second from 15s for each question (need the loop)
+    Create gradeQuiz function
+
+    EVENT LISTENER:
+    Add event listener on START button and SUBMIT button 
+  */
+
 $(document).ready(function(){
+
+$("#timecount").hide();
+$("#game").hide();
+$('#result').hide();
+
+var startGame = $("#start-btn").on('click',function(){
+    $(this).parent().hide(); //hide the start box before user click start
+    $('#timecount').show();
+    $('#remainingTime').show();
+    countdown(32);
+    questionDisplay ();
+
+})
+
+var result = {
+    correct: 0 ,
+    incorrect: 0 ,
+    unanswer: 0
+};
+
+// var timeCount = {
+//     timer: 30,
+//     timerOn: false,
+//     timerId: ''
+// };
 
 var questions = [
 
@@ -10,7 +53,9 @@ var questions = [
                 b: "All of his short" , 
                 c: "His favorite shoes"
             } ,
-            correctAnswer: "All of his blue jeans"
+            correctAnswer: "All of his blue jeans" ,
+            class: 'first' ,
+            divClass: '.exchange'
         } ,
         
         {
@@ -20,7 +65,10 @@ var questions = [
                 b: "A banana" , 
                 c: "A toy gun"
             } , 
-            correctAnswer: "A banana"
+            correctAnswer: "A banana",
+            class: 'second' ,
+            divClass: '.gun'
+            
         } , 
     
         {
@@ -30,7 +78,9 @@ var questions = [
                 b: "Phyll" , 
                 c: "Easy rider" 
             } ,
-            correctAnswer: "Easy rider"
+            correctAnswer: "Easy rider",
+            class: 'third' ,
+            divClass: 'phyllis'
         } , 
     
         {
@@ -40,7 +90,9 @@ var questions = [
                 b: "Tunafish and skittles" , 
                 c: "Mayo and black olives" 
             } ,
-            correctAnswer: "Peanut butter and tunafish"
+            correctAnswer: "Peanut butter and tunafish",
+            class: 'forth' ,
+            divClass: '.sandwich'
         } ,
     
         {
@@ -50,7 +102,9 @@ var questions = [
                 b: "A foot bath" , 
                 c: " M&M’s"
             } ,
-            correctAnswer: "A foot bath"
+            correctAnswer: "A foot bath",
+            class: 'fifth' ,
+            divClass: '.kevin'
         } ,
     
         {
@@ -60,7 +114,9 @@ var questions = [
                 b: "Coffee 3 sugars 5 creams" , 
                 c: "Tea 3 sugar 5 creams"
             } ,
-            correctAnswer: "Iced tea 3 sugars 5 creams"
+            correctAnswer: "Iced tea 3 sugars 5 creams",
+            class: 'sixth' ,
+            divClass: '.stanley'
         } ,
     
         {
@@ -70,7 +126,9 @@ var questions = [
                 b: "Cream and sugar" , 
                 c: "Coffee and sugar" 
             } ,
-            correctAnswer: "Milk and sugar"
+            correctAnswer: "Milk and sugar",
+            class: 'seventh' ,
+            divClass: '.pam'
         } , 
     
         {
@@ -80,7 +138,9 @@ var questions = [
                 b: "MISStery" , 
                 c: "Sassypant" 
             } ,
-            correctAnswer: "MISSsterious"
+            correctAnswer: "MISSsterious",
+            class: 'eighth' ,
+            divClass: '.suit'
         } ,
     
         {
@@ -90,7 +150,9 @@ var questions = [
                 b: "What an awful name" , 
                 c: "I thought Rajnigandha was a boy’s name" 
             } ,
-            correctAnswer: "I thought Rajnigandha was a boy’s name"
+            correctAnswer: "I thought Rajnigandha was a boy’s name",
+            class: 'nineth' ,
+            divClass: '.kelly'
         } ,
     
         {
@@ -100,8 +162,95 @@ var questions = [
                 b: "Ghandi" , 
                 c: "Martin Luther King Jr"
             } ,
-            correctAnswer: "Martin Luther King Jr" 
+            correctAnswer: "Martin Luther King Jr" ,
+            class: 'tenth' ,
+            divClass: '.michael'
         } ];
+
+var labels = ["Question 1:" , "Question 2:" , "Question 3:" , "Question 4:" , "Question 5:" , "Question 6:" , "Question 7:" , "Question 8:" , "Question 9:" , "Question 10:"]
+
+//Question Display function 
+var questionDisplay = function() {
+
+    for (var i=0 ; i < questions.length ; i++) {
+        $("#game").append(' <div class="' + questions[i].name + '"></div> ');
+        $(questions[i].class).append (' <div class="questionTitle"> ' + questions[i].question + ' </div> ');
+    }
+
+    for (var e = 0; e < labels.length , e++) {
+        var shortlabel = labels(e);
+        console.log (labels(e));
+    }
+
+    for (var j = 0 ; j <= question[i].answers.length ; j++ ) {
+    $(questions[i].Class).append( ' <input type="radio" name="' + questions[i].name + '"value=" ' + questions[i].answers[j] + ' "/>' 
+    +'<label for="' + labels[j] + ' "> ' + questions[i].answers[j] + ' </label> ');
+    }
+
+    $('#game').prepend('<hr />');
+   }
+}
+
+//Count down function
+var countdown = function(seconds) {
+    var timer = setInterval(function () {
+        seconds -= 1;
+        $("#remainingTime").html(seconds);
+
+        if (seconds <= 0) {
+            $('#countdown').fadeOut(500);
+            var correct =0;
+            var incorrect =0;
+            var unanswer =0;
+
+        for ( var c=0; c < 10 ; c++) {
+            if ($('input:radio[name="' + questions[c].name + ' "]:checked').val() === questions[c].correct) {
+                correct++;
+                console.log("this is correct! number: " + c);
+
+            } else {
+                incorrect++;
+                console.log("this is wrong: " + c)
+            };
+        }
+        $('#correctAnswer').append(correct);
+        $('#incorrectAnswer').append(incorrect);
+        $('#result').fadeIn(1000).show();
+
+        clearInterval(timer);
+        return;
+    }
+},1000);
+
+$('#submit-btn').on('click',function() {
+    clearInterval(timer);
+})
+
+};
+
+//gradeQuiz function
+
+var gradeQuiz = $('#submit-btn').on('click', function(){
+    var correct = 0;
+    var incorrect =0;
+    var unanswer = 0;
+
+    for (var i =0; i<10; i++) {
+        if($(' input:radio[name=" ' + questions[i].name + ' "]:checked').val() === questions[i].correct) {
+            correct++;
+        } else {
+            incorrect++;
+        }; 
+    };
+    countdown();
+
+    $('#timecount').fadeOut(500);
+    $('#result').show();
+    $('#correctAnswer').append(correct);
+    $('#incorrectAnswer').append(incorrect);
+
+})
+
 
 
 
